61f62393f324161339696aa18cfebf4f5a0c1a22
odikadzegiorgi4@gmail.com <odikadzegiorgi4@gmail.com>
2023-10-21 21:48:43 +0400

    add password check logic

--- src/components/optionsContainer/OptionsContainer.tsx
+++ src/components/optionsContainer/OptionsContainer.tsx
@@ -7,6 +7,10 @@ export const OptionsContainer = () => {
     const [lowerCase, setLowerCase] = useState<boolean>(false)
     const [numbers, setNumbers] = useState<boolean>(false)
     const [symbols, setSymbols] = useState<boolean>(false)
+    const [passwordStrength, setPasswordStrength] = useState<string>('')
+    const [strengthNumber, setStrengthNumber] = useState<number>(0)
+
+    const [color, setColor] = useState<string>('')
 
     const generateRandomPassword = () =>{
         let charSet = ''
@@ -24,6 +28,35 @@ export const OptionsContainer = () => {
         }
 
         setPassword(newPAssword)
+        checkPasswordStrength()
+    }
+
+    const checkPasswordStrength = () =>{
+        let bgColorCheck = 0
+
+        if(upperCase) bgColorCheck += 1
+        if(lowerCase) bgColorCheck += 1
+        if(numbers) bgColorCheck += 1
+        if(symbols) bgColorCheck += 1
+
+        if(bgColorCheck == 1){
+            setPasswordStrength('TOO WEAK')
+           setColor('#A4FFAF')
+        } 
+        if(bgColorCheck == 2){
+            setPasswordStrength('WEAK')
+             setColor('#F64A4A')
+        } 
+        if(bgColorCheck == 3){
+            setPasswordStrength('MEDIUM')
+            setColor('#FB7C58')
+        } 
+        if(bgColorCheck == 4){
+            setPasswordStrength('STRONG')
+            setColor('#F8CD65')
+        } 
+        
+        setStrengthNumber(bgColorCheck)
     }
 
   return (
@@ -45,16 +78,31 @@ export const OptionsContainer = () => {
                 <label>include numbers</label>
             </div>
             <div>
-                <input type="checkbox" onChange={()=> setSymbols(symbols)} />
+                <input type="checkbox" onChange={()=> setSymbols(!symbols)} />
                 <label>include symbol</label>
             </div> 
         </div>
 
         <button onClick={generateRandomPassword}>generate new password</button>
 
+        <div>{passwordStrength}</div>
+
 
         <div className='passwordStrength'>
-            <div className='strengthBox'></div>
+            <div className='strengthBox' style={{
+                backgroundColor: 
+                strengthNumber === 0 ? '' : color}}></div>
+            <div className='strengthBox' style={{
+                backgroundColor: 
+                strengthNumber === 1 ? '' : color}} ></div>
+            <div className='strengthBox' style={{
+                backgroundColor: 
+                strengthNumber === 1 ? ''
+                :strengthNumber === 2 ? '' 
+                :color}}></div>
+            <div className='strengthBox' style={{
+                backgroundColor: 
+                strengthNumber === 4 ? color : ''}}></div>
         </div>
     </div>
   )
